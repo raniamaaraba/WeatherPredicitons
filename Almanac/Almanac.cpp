31bb1f63ba50afe27a6a5950c12cbfeb19905d1f
raniamaaraba <145875065+raniamaaraba@users.noreply.github.com>
2024-04-20 12:38:39 -0400

    Add files via upload

---
+++ Almanac/Almanac.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/** Describe your own personal ideal weather day 
+ * like an almanac and output the ideal weather
+ * This is the ideal weather of Rania Maaraba
+ **/
+using namespace std;
+
+int main () {
+    cout << "My perfect day would occur in the Winter:" << endl;
+    cout << "Precipitation: 100%\nSkies: Snow" << endl;
+    cout << "Temperature: 5 to -5F" << endl;
+    
+    return 0;
+}
